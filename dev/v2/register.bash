4b1839ffd495256718ec8bf13b7af97b5fdf1e7c
xiao.xinmiao <xiaoxm_001@163.com>
2019-09-05 08:34:59 +0800

    #3 dev add srx runner

--- dev/v2/register.bash
+++ dev/v2/register.bash
@@ -33,6 +33,36 @@ sudo gitlab-runner register -n \
    --env "DOCKER_AUTH_CONFIG={\"auths\": {\"registry.elandsystems.cn:5000\": {\"auth\": \"******\"},\"registry.p2shop.com.cn\": {\"auth\": \"******\"}}}" \
    --custom_build_dir-enabled=true  \
    --description "229 p2shop golang build" 
+
+
+# srx  ====
+
+
+sudo gitlab-runner register -n \
+   --url https://gitlab.srxcloud.com/ \
+   --registration-token ****** \
+   --tag-list "golang-test" \
+   --executor docker \
+   --description "229 srx golang test" \
+   --docker-image "docker:19.03.1" \
+   --docker-privileged \
+   --docker-dns  "8.8.8.8" \
+   --env "DOCKER_AUTH_CONFIG={\"auths\": {\"registry.elandsystems.cn:5000\": {\"auth\": \"******\"},\"registry.p2shop.com.cn\": {\"auth\": \"******\"}}}" \
+   --custom_build_dir-enabled=true  
+
+
+sudo gitlab-runner register -n \
+   --url https://gitlab.srxcloud.com/ \
+   --registration-token ****** \
+   --tag-list "golang-build" \
+   --executor shell \
+   --docker-dns  "8.8.8.8" \
+   --env "DOCKER_AUTH_CONFIG={\"auths\": {\"registry.elandsystems.cn:5000\": {\"auth\": \"******\"},\"registry.p2shop.com.cn\": {\"auth\": \"******\"}}}" \
+   --custom_build_dir-enabled=true  \
+   --description "229 srx golang build" 
+
+
+
    
 
     
